65e4c327d94dcdf65572c5be7eb7533d064869f9
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-03-05 14:40:00 +0530

    added heading

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ const App = () => {
   const App = styled.div`
   background: #1c2f2f;
   display:flex;
-  justify-content:center;
+  flex-direction:column;
+  justify-content:space-evenly;
   align-items: center;
   height: 100vh;
   width:100%;
   `
+  const Title = styled.div`
+  color: white;
+  font-size: xx-large;
+  `
+  
   return (
     <App>
+      <Title>Calcultor App</Title>
       <Calculator />
     </App>
   );
