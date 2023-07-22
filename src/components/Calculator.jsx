6c2bcd9ddc415d019d9befee0626face900d29c6
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-07-22 17:10:34 +0530

    layout and separate lines finalized

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,141 +5,171 @@ const row = ["AC","C","%"]
 const col = ["/","*","-","+","="]
 const keys = [7,8,9,4,5,6,3,2,1,"**",0,"."]
 const Calculator = () => {
-  const [val, setVal] = useState('');
+  const [input, setInput] = useState('');
+  const [val, setVal] = useState();
 
   const clickHandler = (e) => {
     let buttonValue = e.target.innerText;
+    console.log("this is input:",input);
     if(buttonValue === "="){
       // eslint-disable-next-line
-      let finalValue = eval(val)
+      let finalValue = eval(input);
+      // console.log("Ek",input)
+      // setInput(input.substring(0,input.length-1));
+      console.log("temp", input,"\n","val",finalValue);
       setVal(finalValue);
     }
     else if(buttonValue === "C"){
-      setVal(val.slice(0,-1))
+      setInput(input.slice(0,-1))
     }
     else if(buttonValue === "AC"){
+      setInput('');
       setVal('');
     }
     else{
-      setVal(val + buttonValue);
+      setInput(input + buttonValue);
     }
   }
-
-  const Calculator = styled.div`
-  height: 500px;
-  width: 350px;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  background: #ebebeb;
-  `
-  const Display = styled.div`
-  height: 200px;
-  width: inherit;
-  position: relative;
-  background: transparent;
-  `
-  const DisplayText = styled.div`
-  color: black;
-  position: absolute;
-  right:0;
-  bottom:0;
-  font-size: 20px;
-  text-align: end;
-  margin: 1rem;
-  `
-  const FlexBoxMain = styled.div`
-  display: flex;
-  `
-  const FlexBoxSub = styled.div`
-  margin: 0rem;
-  flex: 1;
-  `
-  const Row = styled.div`
-  display: flex;
-  `
-  const Keys =styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  `
-  const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  flex: 0.25;
-  `
-  const ColButtons = styled.button`
-  font-size: 20px;
-  box-shadow: none;
-  border: none;
-  height: 3.5rem;
-  flex: 1;
-  background: #ebebeb;
-  `
-  const Buttons = styled.button`
-  flex: 1 0 33%;
-  font-size: 20px;
-  box-shadow: none;
-  border: none;
-  height: 3.5rem;
-  background: #ebebeb;
-  align-items:center;
-  `
-  const CoreButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
-  height: 2.5rem;
-  width: 2.5rem;
-  border-radius: 2rem;
-  border: 1px solid transparent;
-  background: #ffffff;
-  `
-  const SideButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
-  height: 2.5rem;
-  width: 2.5rem;
-  border-radius: 2rem;
-  border: 1px solid transparent;
-  background: #d6d6d6;
-  `
   
   return(
-    <Calculator>
+    <CalculatorBox>
       <Display>
-        <DisplayText >{val}</DisplayText>
+        <DisplayBox >
+          <DisplayText>
+            <InputVal>{input}</InputVal>
+            <OutputVal>{val}</OutputVal>
+          </DisplayText>
+        </DisplayBox>
       </Display>
       <FlexBoxMain>
         <FlexBoxSub>
           <Row>
             {row.map((r)=>(
-              <Buttons onClick={clickHandler}>
-                <SideButton>{r}</SideButton>
-              </Buttons>
+              <WrapButtons onClick={clickHandler}>
+                <SideBgButton>{r}</SideBgButton>
+              </WrapButtons>
             ))}
           </Row>
           <Keys>
             {keys.map((k)=>(
-              <Buttons onClick={clickHandler}>
-                <CoreButton>{k}</CoreButton>
-              </Buttons>
+              <WrapButtons onClick={clickHandler}>
+                <NoBgButton>{k}</NoBgButton>
+              </WrapButtons>
             ))}
           </Keys>
         </FlexBoxSub>
         <Column>
           {col.map((c)=>(
-             <ColButtons onClick={clickHandler}>
-              <SideButton>{c}</SideButton>
-             </ColButtons>
+            <ColButtons onClick={clickHandler}>
+              <SideBgButton>{c}</SideBgButton>
+            </ColButtons>
           ))}
         </Column>
       </FlexBoxMain>
-    </Calculator>
+    </CalculatorBox>
   )
 }
 
-export default Calculator
+const CalculatorBox = styled.div`
+height: 450px;
+width: 350px;
+margin: auto;
+display: flex;
+flex-direction: column;
+justify-content: center;
+background: #e0e0e0;
+border-radius: 1rem;
+`
+const Display = styled.div`
+display: flex;
+margin: auto;
+height: 90px;
+width: inherit;
+background: transparent;
+border-radius: 1rem 1rem 0 0;
+font-family: 'Orbitron', sans-serif;
+`
+const DisplayBox = styled.div`
+width: 300px;
+height: 70px;
+overflow-wrap: break-word;
+color: black;
+text-align: right;
+margin: auto;
+border: 1px solid black;
+border-radius: 1rem;
+padding: 0.5rem;
+`
+const DisplayText = styled.div`
+width: inherit;
+height: inherit;
+margin: auto;
+position: relative;
+`
+const InputVal = styled.div`
+font-size: 18px;
+`
+const OutputVal = styled.div`
+font-size: 22px;
+position: absolute;
+bottom: 0;
+right: 0;
+`
+const FlexBoxMain = styled.div`
+display: flex;
+margin: auto;
+`
+const FlexBoxSub = styled.div`
+margin: 0rem;
+flex: 1;
+`
+const Row = styled.div`
+display: flex;
+`
+const Keys =styled.div`
+display: flex;
+flex-wrap: wrap;
+`
+const Column = styled.div`
+display: flex;
+flex-direction: column;
+flex-wrap: wrap;
+flex: 0.25;
+`
+const ColButtons = styled.button`
+font-size: 18px;
+box-shadow: none;
+height: 3.5rem;
+flex: 1;
+background: transparent;
+border: 1px solid transparent;
+border-radius: 1rem;
+font-family: 'Orbitron', sans-serif;
+`
+const WrapButtons = styled.button`
+flex: 1 0 33%;
+font-size: 18px;
+box-shadow: none;
+border: 1px solid transparent;
+border-radius: 1rem;
+height: 3.5rem;
+background: transparent;
+align-items:center;
+font-family: 'Orbitron', sans-serif;
+`
+const NoBgButton = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin: auto;
+height: 2.5rem;
+width: 2.5rem;
+border-radius: 2rem;
+border: 1px solid transparent;
+background: #f5f5f5;
+`
+const SideBgButton = styled(NoBgButton)`
+background: #cccccc;
+`
+
+export default Calculator;
