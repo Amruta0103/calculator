eb522e1abc88daaab1b7d3a603646d3e6812384d
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-07-22 17:38:19 +0530

    Final touches done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import styled from 'styled-components';
 import Calculator from './components/Calculator';
+import logo from './assets/calculator-simple.svg';
 // import Temp from './temp';
 
 const App = () => {
@@ -8,17 +9,37 @@ const App = () => {
   background: #1c2f2f;
   display:flex;
   flex-direction:column;
-  justify-content:space-evenly;
+  justify-content: center;
   align-items: center;
   height: 100vh;
   width:100%;
   `
-  
+  const Header = styled.div`
+  color: #f5f5f5;
+  margin: auto;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center
+  ` 
+  const Logo = styled.img`
+  height: 20px;
+  width: 20px;
+  margin: 0 0.8rem 0;
+  `
+  const Footer = styled.div`
+  color: #f5f5f5;
+  margin: auto;
+  ` 
+
   return (
     <App>
-      {/* <Title>Simply-Calcy</Title> */}
+      <Header>
+        <Logo src={logo} />
+        Simply Calcy
+      </Header>
       <Calculator />
-      {/* <Temp /> */}
+      <Footer>Made with ♡ by Amruta D.</Footer>
     </App>
   );
 }
